220d2734971174780bc0106f3b87f8ad37ca2576
james etchberger <etchbergerjames@gmail.com>
2022-08-23 11:00:46 -0400

    image cropping is live and fixed a typo

--- src/renderer/components/Photo.tsx
+++ src/renderer/components/Photo.tsx
@@ -1,14 +1,17 @@
 import { useState } from 'react';
+import Cropper from 'react-easy-crop';
 import { readFile } from '../../helpers/images';
 
 export default function Photo() {
   const [imageSrc, setImageSrc] = useState(null); // file data
-  const [fileName, setFileName] = useState(null); // file address
+  const [crop, setCrop] = useState({ x: 0, y: 0 })
+  const [zoom, setZoom] = useState(1)
+  // const [fileName, setFileName] = useState(null); // file address
   const handleFileChange = async (e: any) => {
     if (e.target.files && e.target.files.length) {
       //we got a file
-      const file = e.traget.files[0];
-      setFileName(file.path);
+      const file = e.target.files[0];
+      // setFileName(file.path);
       //get the image data from the file
       const imageData: any = await readFile(file)
       //set imageSrc to that data
@@ -25,7 +28,13 @@ export default function Photo() {
   }
   return (
     <>
-      <img src={imageSrc} />
+      <Cropper
+      image={imageSrc}
+      crop={crop}
+      zoom={zoom}
+      onCropChange={setCrop}
+      onZoomChange={setZoom}
+      />
     </>
   )
 }
