cf150fa9d45c2333dc628b1548abc536e4cf44db
james etchberger <etchbergerjames@gmail.com>
2022-08-23 10:48:16 -0400

    added a way to select photos from computer

--- src/renderer/components/Photo.tsx
+++ src/renderer/components/Photo.tsx
@@ -1,3 +1,31 @@
+import { useState } from 'react';
+import { readFile } from '../../helpers/images';
+
 export default function Photo() {
-  return <h1>Photo Cropper goes here</h1>
+  const [imageSrc, setImageSrc] = useState(null); // file data
+  const [fileName, setFileName] = useState(null); // file address
+  const handleFileChange = async (e: any) => {
+    if (e.target.files && e.target.files.length) {
+      //we got a file
+      const file = e.traget.files[0];
+      setFileName(file.path);
+      //get the image data from the file
+      const imageData: any = await readFile(file)
+      //set imageSrc to that data
+      setImageSrc(imageData)
+    }
+  };
+  if (!imageSrc) {
+    return (
+      <>
+        <h1>Please choose photo to crop</h1>
+        <input type="file" accept="image/*" onChange={handleFileChange} />
+      </>
+    );
+  }
+  return (
+    <>
+      <img src={imageSrc} />
+    </>
+  )
 }
